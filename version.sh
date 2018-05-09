89d04163d4f6fb2c36874e6653e9688dd59c39f7
Urban Wallasch <info@latice.de>
2018-05-09 13:12:34 +0200

    * Added git support to version number generation.

--- version.sh
+++ version.sh
@@ -4,7 +4,10 @@ VF=$2
 TAG=$3
 TF=`tempfile`
 RV=`head -n1 version.in`
-SV=`svnversion | grep -E '^[0-9]+[^\s]*' || echo 'Unknown'`
+SV="$(svnversion | grep -E '^[0-9]+[^\s]*' | tr ':' '_' )"
+if [ -z "$SV" ]; then
+    SV="git$(git show -s --pretty=format:%h)"
+fi
 echo "$RV" > "$VF"
 echo "#define SVNVER \"$SV$TAG\"" >> "$VF"
 cat "$VF"
